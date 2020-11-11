8d19fdb2a2116d5d58d41ef263ae800e93752ec0
Konstantin Musikhin <allcodesmail@gmail.com>
2020-11-11 22:05:33 +0400

    Добавьте файлы проекта.

---
+++ KosMVC/Controllers/NumberController.cs
@@ -0,0 +1,35 @@
+﻿using KosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KosMVC.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class NumberController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> NumberToWords(ulong number)
+        {
+
+            var client = new ServiceNumberConversion.NumberConversionSoapTypeClient(
+                    ServiceNumberConversion.NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap12
+                );
+            var response = await client.NumberToWordsAsync(number);
+            return Ok(new NumberWordResult { text = response.Body.NumberToWordsResult });
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NumberToDollars(ulong number)
+        {
+            var client = new ServiceNumberConversion.NumberConversionSoapTypeClient(
+                   ServiceNumberConversion.NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap12
+               );
+            var response = await client.NumberToDollarsAsync(number);
+            return Ok(new NumberWordResult { text = response.Body.NumberToDollarsResult });
+        }
+       
+    }
+}
---
+++ KosMVC/Models/NumberWordResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KosMVC.Models
+{
+    public class NumberWordResult
+    {
+        public string text {get; set;}
+    }
+}
